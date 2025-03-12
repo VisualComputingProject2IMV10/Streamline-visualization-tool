2d65b44d6d7d668a546daff74edfda223f0b4d4e
ThijsEgberts <t.t.a.egberts@student.tue.nl>
2025-03-12 15:47:41 +0100

    Wireframe square

--- testProj/Source.cpp
+++ testProj/Source.cpp
@@ -45,17 +45,32 @@ int main(void)
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
 
-    Shader shader1("VertexShader1.vert", "FragShader1.frag");
+    Shader shader1("VertexShader1.vs", "FragShader1.fs");
 
-    float vertices[] = {
+    /*float vertices[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
         0.0f, 0.5f, 0.0f
+    };*/
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f
     };
+    unsigned int indices[] = {
+        0,1,2,
+        1,2,3
+    };
+
+    //create elements buffer
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
     //create vertex buffer
     unsigned int VBO;
-    glad_glGenBuffers(1, &VBO);
+    glGenBuffers(1, &VBO);
 
     //create vertex array
     unsigned int VAO;
@@ -63,8 +78,12 @@ int main(void)
 
     //bind vertex array and buffer
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //set vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -84,7 +103,13 @@ int main(void)
         shader1.use();
         glBindVertexArray(VAO);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        
+        glBindVertexArray(0);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
